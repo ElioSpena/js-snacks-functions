7e2669c8a7f0a7d672f644c511560eecb73a72aa
ElioSpena <elio.spena.94@gmail.com>
2025-09-26 12:33:53 +0200

    fix: correzione esercizio 1

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,23 +1,22 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-const divider = '","';
-
 
 // Dichiara la funzione qui.
 
-function firstWord(name, divider) {
+function firstWord(name) {
     let result = [];
     for (let i = 0; i < name.length; i++) {
         const curElement = name[i];
-        result += curElement[0] + divider;
+        const FirstWord = curElement[0];
+        result[i] = FirstWord;
     }
     return result;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-console.log(firstWord(names, divider));
+console.log(firstWord(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,13 +1,26 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const divider = '","';
 
 // Dichiara la funzione qui.
+
+function firstLetterAs(name) {
+    let result = "";
+    for (let i = 0; i < name.length; i++) {
+        const curWord = name[i];
+        if (curWord[0] === "A") {
+            result += curWord + divider;
+        } else {
+
+        }
+    }
+    return result;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(firstLetterAs(names))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]

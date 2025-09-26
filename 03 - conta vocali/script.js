8723a584c610dda3b8a0b3f7e998fd95b5bb1372
ElioSpena <elio.spena.94@gmail.com>
2025-09-26 14:19:38 +0200

    feat: esercizio 3 completo

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,21 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function isVowels(string) {
+    let result = 0;
+
+    for (i = 0; i < string.length; i++) {
+        let curLetter = string[i];
+        if (curLetter === "a" || curLetter === "e" || curLetter === "i" || curLetter === "o" || curLetter === "u") {
+            result++;
+        }
+    }
+    return result
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(isVowels(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
